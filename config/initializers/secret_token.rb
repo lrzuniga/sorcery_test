cb00fcdae10276008f1808bcd9cc9ca0220b634e
lrzuniga <lrzuniga@gmail.com>
2013-09-20 13:54:28 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SorceryTest::Application.config.secret_key_base = '4bfd3df59cfd9cca1fca6e31267cd7d8d2ae46e8e459fc5532d43a457c237459fb0d0e94af049ae736708cda59f7e1bd3f081e199b31f3a4aefc1b5f6ce14567'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SorceryTest::Application.config.session_store :cookie_store, key: '_sorcery_test_session'
